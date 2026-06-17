9e0b409a48422189dc60f143bb82e7c23ed251bb
federicoduprat-ai <federico.duprat@obel.la>
2026-06-17 16:44:50 -0300

    Add GTM and Meta Pixel tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,8 +22,15 @@ export default function RootLayout({
     <html lang="es" className={`${geistSans.variable} antialiased`}>
       <head>
         <link rel="icon" type="image/png" href="/favicon.png" />
+        {/* Google Tag Manager */}
+        <script dangerouslySetInnerHTML={{ __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','GTM-MF3328S3');` }} />
+        {/* Meta Pixel */}
+        <script dangerouslySetInnerHTML={{ __html: `!function(f,b,e,v,n,t,s){if(f.fbq)return;n=f.fbq=function(){n.callMethod?n.callMethod.apply(n,arguments):n.queue.push(arguments)};if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';n.queue=[];t=b.createElement(e);t.async=!0;t.src=v;s=b.getElementsByTagName(e)[0];s.parentNode.insertBefore(t,s)}(window,document,'script','https://connect.facebook.net/en_US/fbevents.js');fbq('init','964015321769147');fbq('track','PageView');` }} />
+        <noscript dangerouslySetInnerHTML={{ __html: `<img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id=964015321769147&ev=PageView&noscript=1"/>` }} />
       </head>
       <body className="min-h-full flex flex-col">
+        {/* Google Tag Manager (noscript) */}
+        <noscript dangerouslySetInnerHTML={{ __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MF3328S3" height="0" width="0" style="display:none;visibility:hidden"></iframe>` }} />
         {children}
         <a
           href="https://wa.me/5491128592763?text=Hola%21%20Quiero%20m%C3%A1s%20informaci%C3%B3n%20sobre..."
